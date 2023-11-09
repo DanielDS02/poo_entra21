5763e25e84870ac29993effe6f7a4b0634d3446e
DanielDS02 <danieldoleski02@gmail.com>
2023-11-08 22:28:01 -0300

    commit 3

---
+++ aula 1/exercicio2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Quantos graus em °C deseja converter?");
+var celsius = Convert.ToInt32(Console.ReadLine());
+
+var fahrenheit = (celsius* 9 / 5) + 32;
+var kelvin = celsius + 273.15;
+
+Console.WriteLine( celsius + " graus celsius são " + fahrenheit + " °F e " + kelvin +" Kelvin");
