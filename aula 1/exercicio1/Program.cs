c7a6f34ee789bb8adc14457e4aa9b7a2f1111982
Daniel <danieldoleski02@gmail.comm>
2023-11-10 15:15:36 -0300

    commit 4

--- aula 1/exercicio1/Program.cs
+++ aula 1/exercicio1/Program.cs
@@ -1,4 +1,22 @@
-﻿
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    Console.WriteLine("Quantas vezes você quer que seja calculado o fibonacci?");
+    var repeticoes = Convert.ToInt32(Console.ReadLine());
+
+    int numeroAnterior = 0;
+    int numeroAtual = 1;
+    int novoNumero;
+    int fibonacci;
 
 
-
+    for (int i = 0; i < repeticoes; i++)
+    {
+      fibonacci = numeroAnterior + numeroAtual;
+      Console.WriteLine(fibonacci);
+      numeroAnterior = numeroAtual;
+      numeroAtual = fibonacci;
+    }
+  }
+}
--- aula 1/exercicio2/Program.cs
+++ aula 1/exercicio2/Program.cs
@@ -1,7 +1,12 @@
-﻿Console.WriteLine("Quantos graus em °C deseja converter?");
-var celsius = Convert.ToInt32(Console.ReadLine());
+﻿internal class Program
+{
+    private static void Main(string[] args, Console console)
+    {
+        Console.WriteLine("Quantos graus em °C deseja converter?");
+        var celsius = Convert.ToInt32(Console.ReadLine());
 
-var fahrenheit = (celsius* 9 / 5) + 32;
-var kelvin = celsius + 273.15;
-
-Console.WriteLine( celsius + " graus celsius são " + fahrenheit + " °F e " + kelvin +" Kelvin");
+        var fahrenheit = celsius * 9 / 5 + 32;
+        var kelvin = celsius + 273.15;
+        console.WriteLine(celsius + " graus celsius são " + fahrenheit + " °F e " + kelvin + " Kelvin");
+    }
+}
