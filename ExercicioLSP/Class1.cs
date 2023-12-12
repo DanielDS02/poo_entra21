08a34748b610eec29a75312bb1764a1afb818a9e
Daniel <danieldoleski02@gmail.comm>
2023-12-11 21:30:16 -0300

    alterações exercicios

---
+++ ExercicioLSP/Class1.cs
@@ -0,0 +1,36 @@
+﻿public class GuardaChuva
+{
+    public virtual void Abrir()
+    {
+        Console.WriteLine("Guarda-chuva aberto.");
+    }
+    public virtual void Fechar()
+    {
+        Console.WriteLine("Guarda-chuva fechado.");
+    }
+}
+public class GuardaChuvaAutomatico : GuardaChuva
+{
+    private bool _estaAberto;
+    public void NewAbrir()
+    {
+        Console.WriteLine("Guarda-chuva automático abrindo.");
+        _estaAberto = true;
+    }
+    public void NewFechar()
+    {
+        Console.WriteLine("Guarda-chuva automático fechando.");
+        _estaAberto = false;
+    }
+    public void Agitar()
+    {
+        if (_estaAberto)
+        {
+            Console.WriteLine("Guarda-chuva automático agitado.");
+        }
+        else
+        {
+            Console.WriteLine("Não é possível agitar um guarda-chuva fechado.");
+        }
+    }
+}
---
+++ ExercicioOCP/Class1.cs
@@ -0,0 +1,53 @@
+﻿public class Carro
+{
+    public string Modelo { get; set; }
+    public int Ano { get; set; }
+}
+public class CalculadoraAluguel
+{
+    public double CalcularPrecoAluguel(Carro carro)
+    {
+        double preco = 0;
+        if (carro.Ano >= 2020)
+        {
+            preco = 100;
+        }
+        else
+        {
+            preco = 50;
+        }
+        return preco;
+    }
+}
+public class CarroElétrico : Carro 
+{ 
+    public int AutonomiaBateria { get; set; } 
+
+    public int Bateria { get; set;}
+
+    public CarroElétrico()
+    {
+        
+    }
+}
+public class CalculadoraAluguelElétrico
+{
+    public double CalcularPrecoAluguel(Carro carro, CarroElétrico carroEletrico)
+    {
+        double preco = 0;
+        if (carro.Ano >= 2020)
+        { preco = 100; }
+        else if (carro is CarroElétrico)
+        {
+            if (carroEletrico.AutonomiaBateria > 300)
+            {
+                preco += 50;
+            }
+        }
+        else
+        {
+            preco = 50;
+        }
+        return preco;
+    }
+}
